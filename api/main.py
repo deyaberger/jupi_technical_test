ff0a661fc861ed8398f9608bb8a2f5ff158479c6
deya.berger <d.berger@axione.fr>
2024-09-08 14:59:23 +0200

    feat(back): mocking execution time

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import random
 import string
+import time
 
 app = FastAPI()
 
@@ -8,6 +9,7 @@ app = FastAPI()
 async def get_suggestions(query: str):
     suggestions = []
     for _ in range(5):
+        time.sleep(random.randint(0, 5))
         random_suffix = ''.join(random.choices(string.ascii_lowercase, k=5))
         suggestions.append(query + random_suffix)
     return {"suggestions": suggestions}
